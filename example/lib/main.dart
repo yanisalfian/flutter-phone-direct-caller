07d3889ff2e9f710c7237c8f6b1e22327e681589
yanisalfian <yanisalfian@gmail.com>
2018-11-21 11:49:54 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,32 +12,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  TextEditingController _numberCtrl = new TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterPhoneDirectCaller.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    _numberCtrl.text = "085921191121";
   }
 
   @override
@@ -49,11 +29,20 @@ class _MyAppState extends State<MyApp> {
         ),
         body: new Column(
           children:<Widget>[
-            new Text('Running on: $_platformVersion\n'),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _numberCtrl,
+                decoration: InputDecoration(
+                  labelText: "Phone Number"
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
             RaisedButton(
               child: Text("Test Call"),
               onPressed: () async{
-                FlutterPhoneDirectCaller.callNumber("085921191121");
+                FlutterPhoneDirectCaller.callNumber(_numberCtrl.text);
               },
             )
           ]
