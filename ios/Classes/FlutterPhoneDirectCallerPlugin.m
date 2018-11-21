3fe0d4030ede97d7776d0f34a27ac66b4ea1c617
yanisalfian <yanisalfian@gmail.com>
2018-11-21 11:26:06 +0700

    add support for ios

--- ios/Classes/FlutterPhoneDirectCallerPlugin.m
+++ ios/Classes/FlutterPhoneDirectCallerPlugin.m
@@ -13,10 +13,26 @@
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else if ([@"callNumber" isEqualToString:call.method]) {
-             result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+      NSString* number = call.arguments[@"number"];
+      result(@([self directCall:number]));
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (BOOL)directCall:(NSString*)number {
+    number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    if( ! [number hasPrefix:@"tel:"]){
+        number =  [NSString stringWithFormat:@"tel:%@", number];
+    }
+    if(![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:number]]) {
+        return NO;
+    } else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
+        // missing phone number
+        return NO;
+    } else {
+        return YES;
+    }
 }
 
 @end
