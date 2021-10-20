547da790f26c2eb31585fd08b6817af9f5f48367
Amir <developer1398@gmail.com>
2021-10-20 14:28:31 +0400

    Migration to Android v2 embedding done

--- android/src/main/java/com/yanisalfian/flutterphonedirectcaller/FlutterPhoneDirectCallerPlugin.java
+++ android/src/main/java/com/yanisalfian/flutterphonedirectcaller/FlutterPhoneDirectCallerPlugin.java
@@ -7,23 +7,22 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.telephony.TelephonyManager;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.util.Log;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.JSONMethodCodec;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.BinaryMessenger;
-
-import androidx.annotation.NonNull;
 
 /**
  * FlutterPhoneDirectCallerPlugin
@@ -33,97 +32,56 @@ public class FlutterPhoneDirectCallerPlugin implements FlutterPlugin, ActivityAw
     public static final int PERMISSION_DENIED_ERROR = 20;
     public static final String CALL_PHONE = android.Manifest.permission.CALL_PHONE;
     private String number;
-    private Result flutterResult;
 
     private static final String CHANNEL_QUERY = "flutter_phone_direct_caller";
-    private static final String TAG = "phone_direct_caller";
-    private ActivityPluginBinding activityPluginBinding;
+    private MethodChannel channel;
     private Activity activity;
-    private Context ctx;
-
-
-//    /**
-//     * Constructor
-//     */
-//    private FlutterPhoneDirectCallerPlugin(Registrar registrar) {
-//        this.registrar = registrar;
-//    }
+    private Context context;
 
     /**
-     * Plugin registration. Android V1 embedding
+     * Plugin registration.
      */
     public static void registerWith(Registrar registrar) {
         final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_phone_direct_caller");
         FlutterPhoneDirectCallerPlugin flutterPhoneDirectCallerPlugin = new FlutterPhoneDirectCallerPlugin();
-        flutterPhoneDirectCallerPlugin.activity = registrar.activity();
         channel.setMethodCallHandler(flutterPhoneDirectCallerPlugin);
         registrar.addRequestPermissionsResultListener(flutterPhoneDirectCallerPlugin);
     }
 
-    /** Plugin registration. Android V2 embedding */
-    private void init(BinaryMessenger binaryMessenger, Context applicationContext) {
-        Log.d(TAG, "init. Messanger:" + binaryMessenger + " Context:" + applicationContext);
-        final MethodChannel channel = new MethodChannel(binaryMessenger, CHANNEL_QUERY);
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+        Log.e("PhoneDirectCaller", "onAttachedToEngine");
+        this.context = binding.getApplicationContext();
+        channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL_QUERY);
         channel.setMethodCallHandler(this);
-        ctx = applicationContext;
     }
 
     @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        Log.d(TAG, "onAttachedToEngine");
-        init(flutterPluginBinding.getBinaryMessenger(), flutterPluginBinding.getApplicationContext());
+    public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+        Log.e("DirectCaller", "onDeAttachFromEngine");
+        channel.setMethodCallHandler(null);
     }
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        //NO-OP
-        Log.d(TAG, "onDetachedFromEngine");
-    }
-
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-        this.activityPluginBinding = activityPluginBinding;
-        activityPluginBinding.addRequestPermissionsResultListener(this);
-        Log.d(TAG, "onAttachedToActivity");
-    }
-
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-        Log.d(TAG, "onDetachedFromActivityForConfigChanges");
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
-        Log.d(TAG, "onReattachedToActivityForConfigChanges");
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        Log.d(TAG, "onDetachedFromActivity");
-        if (activityPluginBinding != null) {
-            activityPluginBinding.removeRequestPermissionsResultListener(this);
-            activityPluginBinding = null;
-        }
-    }
-
-    /**
-     * Main plugin methods
-     */
     @Override
     public void onMethodCall(MethodCall call, Result result) {
-        flutterResult = result;
-        if (call.method.equals("callNumber")) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("callNumber")) {
             this.number = call.argument("number");
             Log.d("Caller", "Message");
             this.number = this.number.replaceAll("#", "%23");
             if (!this.number.startsWith("tel:")) {
                 this.number = String.format("tel:%s", this.number);
             }
-            if (getPermissionStatus() != 1) {
-                requestsPermission();
+//            if (getPermissionStatus() != 1) {
+//                requestsPermission();
+//            } else {
+            if (callNumber(this.number)) {
+                result.success(true);
             } else {
-                result.success(callNumber(this.number));
+                result.success(false);
             }
+//            }
         } else {
             result.notImplemented();
         }
@@ -150,8 +108,8 @@ public class FlutterPhoneDirectCallerPlugin implements FlutterPlugin, ActivityAw
         try {
             Intent intent = new Intent(isTelephonyEnabled() ? Intent.ACTION_CALL : Intent.ACTION_VIEW);
             intent.setData(Uri.parse(number));
-
-            activity.startActivity(intent);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
 
             return true;
         } catch (Exception e) {
@@ -167,17 +125,38 @@ public class FlutterPhoneDirectCallerPlugin implements FlutterPlugin, ActivityAw
 
     @Override
     public boolean onRequestPermissionsResult(int requestCode, String[] strings, int[] ints) {
+        for (int r : ints) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                return false;
+            }
+        }
         switch (requestCode) {
             case CALL_REQ_CODE:
-                for (int r : ints) {
-                    if (r == PackageManager.PERMISSION_DENIED) {
-                        flutterResult.success(false);
-                        return false;
-                    }
-                }
-                flutterResult.success(callNumber(this.number));
+                callNumber(this.number);
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        Log.e("PhoneDirectCaller", "onAttachedToActivity");
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        Log.e("PhoneDirectCaller", "onDeAttachedFromActivity");
+        activity = null;
     }
 }
